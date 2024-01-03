0bce943f792b1d3010d269572c54a891c30cf195
陈先程 <1090113499@qq.con>
2024-01-03 17:04:07 +0800

    改变路由模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,9 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import {
+  createRouter,
+  createWebHashHistory,
+  RouteRecordRaw,
+  createWebHistory,
+} from 'vue-router'
 import { system } from './system'
 
 const routes: Array<RouteRecordRaw> = [
@@ -18,7 +23,7 @@ const routes: Array<RouteRecordRaw> = [
 
 export const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHashHistory(),
+  history: createWebHistory('/admin'),
   routes, // `routes: routes` 的缩写
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite' //自动导入依赖插件
 import { resolve } from 'path' // 主要用于alias文件路径别名
 
@@ -11,44 +11,48 @@ function pathResolve(dir: string) {
   return resolve(__dirname, '.', dir)
 }
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    WindiCSS(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        {
-          '@/api/callApi': ['callApi'],
-        },
-      ],
-      dts: 'src/types/auto-imports.d.ts', // 依赖表
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const config = loadEnv(mode, './')
 
-  resolve: {
-    alias: {
-      '@': pathResolve('./src'),
-    },
-  },
-  server: {
-    open: '/',
-    cors: true,
-    port: 8090,
+  return {
+    base: './',
+    plugins: [
+      vue(),
+      WindiCSS(),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          {
+            '@/api/callApi': ['callApi'],
+          },
+        ],
+        dts: 'src/types/auto-imports.d.ts', // 依赖表
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
 
-    proxy: {
-      // http://47.97.154.67
-      '/api/': {
-        target: 'http://127.0.0.1:3000/api/',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+    resolve: {
+      alias: {
+        '@': pathResolve('./src'),
       },
     },
-  },
+    server: {
+      open: '/',
+      cors: true,
+      port: 8090,
+
+      proxy: {
+        // http://47.97.154.67
+        '/api/': {
+          target: config.VITE_BASE_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
